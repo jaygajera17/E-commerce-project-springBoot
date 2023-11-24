218aefefdffd81d88bd395cb6a690f6d4bfe114f
rparyian <roman.paryian@gmail.com>
2023-11-24 23:05:59 +0400

    Edit scope of embed-tomcat,extend SpringBootServletInitializer in main class

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -3,9 +3,15 @@ package com.jtspringproject.JtSpringProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-public class JtSpringProjectApplication {
+public class JtSpringProjectApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JtSpringProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JtSpringProjectApplication.class, args);
