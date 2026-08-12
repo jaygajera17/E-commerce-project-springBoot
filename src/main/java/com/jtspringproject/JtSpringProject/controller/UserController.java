645778916921af5a7b31bebdc936ff8eb99b4090
NasarImam <imamnasar26@gmail.com>
2026-08-13 03:29:25 +0530

    Fix user profile authorization

--- src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -2,8 +2,12 @@ package com.jtspringproject.JtSpringProject.controller;
 
 import com.jtspringproject.JtSpringProject.models.Product;
 import com.jtspringproject.JtSpringProject.models.User;
+import com.jtspringproject.JtSpringProject.services.productService;
+import com.jtspringproject.JtSpringProject.services.userService;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
+import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,9 +20,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.jtspringproject.JtSpringProject.services.userService;
-import com.jtspringproject.JtSpringProject.services.productService;
 
 @Controller
 public class UserController {
@@ -45,17 +46,25 @@ public class UserController {
 	@GetMapping("/login")
 	public ModelAndView userLogin(@RequestParam(required = false) String error) {
 		ModelAndView mv = new ModelAndView("userLogin");
+
 		if ("true".equals(error)) {
 			mv.addObject("msg", "Please enter correct email and password");
 		}
+
 		return mv;
 	}
 
 	@GetMapping("/")
 	public ModelAndView indexPage() {
 		ModelAndView mView = new ModelAndView("index");
-		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		String username = SecurityContextHolder
+				.getContext()
+				.getAuthentication()
+				.getName();
+
 		mView.addObject("username", username);
+
 		List<Product> products = this.productService.getProducts();
 
 		if (products.isEmpty()) {
@@ -63,12 +72,12 @@ public class UserController {
 		} else {
 			mView.addObject("products", products);
 		}
+
 		return mView;
 	}
 
 	@GetMapping("/user/products")
 	public ModelAndView getProducts() {
-
 		ModelAndView mView = new ModelAndView("uproduct");
 
 		List<Product> products = this.productService.getProducts();
@@ -84,23 +93,33 @@ public class UserController {
 
 	@PostMapping("newuserregister")
 	public ModelAndView registerNewUser(@ModelAttribute User user) {
+
 		boolean exists = this.userService.checkUserExists(user.getUsername());
 
 		if (!exists) {
 			user.setRole("ROLE_NORMAL");
 			this.userService.addUser(user);
+
 			return new ModelAndView("userLogin");
-		} else {
-			ModelAndView mView = new ModelAndView("register");
-			mView.addObject("msg", user.getUsername() + " is taken. Please choose a different username.");
-			return mView;
 		}
+
+		ModelAndView mView = new ModelAndView("register");
+		mView.addObject(
+				"msg",
+				user.getUsername() + " is taken. Please choose a different username."
+		);
+
+		return mView;
 	}
 
 	@GetMapping("/profileDisplay")
 	public String profileDisplay(Model model) {
 
-		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		String username = SecurityContextHolder
+				.getContext()
+				.getAuthentication()
+				.getName();
+
 		User user = userService.getUserByUsername(username);
 
 		if (user != null) {
@@ -117,25 +136,43 @@ public class UserController {
 	}
 
 	@PostMapping("/updateuser")
-	public String updateUserProfile(@RequestParam("userid") int userid,
+	public String updateUserProfile(
+			@RequestParam("userid") int userid,
 			@RequestParam("username") String username,
 			@RequestParam("email") String email,
 			@RequestParam("password") String password,
-			@RequestParam("address") String address) {
-		User updatedUser = this.userService.updateUserProfile(userid, username, email, password, address);
+			@RequestParam("address") String address,
+			Principal principal) throws AccessDeniedException {
+
+		User updatedUser = this.userService.updateUserProfile(
+				userid,
+				principal.getName(),
+				username,
+				email,
+				password,
+				address
+		);
+
 		if (updatedUser != null) {
 			refreshAuthenticatedPrincipal(username);
 		}
+
 		return "redirect:/";
 	}
 
 	private void refreshAuthenticatedPrincipal(String username) {
-		Authentication currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
-		Authentication newAuthentication = new UsernamePasswordAuthenticationToken(
-				username,
-				currentAuthentication.getCredentials(),
-				currentAuthentication.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(newAuthentication);
-	}
 
+		Authentication currentAuthentication =
+				SecurityContextHolder.getContext().getAuthentication();
+
+		Authentication newAuthentication =
+				new UsernamePasswordAuthenticationToken(
+						username,
+						currentAuthentication.getCredentials(),
+						currentAuthentication.getAuthorities()
+				);
+
+		SecurityContextHolder.getContext()
+				.setAuthentication(newAuthentication);
+	}
 }
